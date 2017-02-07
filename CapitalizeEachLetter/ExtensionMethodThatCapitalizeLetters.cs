950eae057c3b82d265713f5e2ccb4fcf78baf3bc
IvMollov <mollov.i@abv.bg>
2017-02-07 19:04:34 +0200

    Lambda

--- CapitalizeEachLetter/ExtensionMethodThatCapitalizeLetters.cs
+++ CapitalizeEachLetter/ExtensionMethodThatCapitalizeLetters.cs
@@ -8,6 +8,39 @@ namespace CapitalizeEachLetter
 {
    public static class ExtensionMethodThatCapitalizeLetters
     {
-        public static string CapitalizeLetters(this String )
+        public static string CapitalizeLetters(this String text)
+        {
+            char[] newText = text.ToCharArray();
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentNullException("Argument is null or empty");
+            }
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentException("Argument is not valid");
+            }
+            if (newText.Length >= 1)
+            {
+                if (char.IsLower(newText[0]))
+                {
+                    newText[0] = char.ToUpper(newText[0]);
+                }
+
+            }
+
+            for (int i = 1; i < newText.Length; i++)
+            {
+
+                if (newText[i - 1] == ' ')
+                {
+                    if (char.IsLower(text[i]))
+                    {
+                        newText[i] = char.ToUpper(newText[i]);
+                    }
+                }
+            }
+
+            return new String(newText);
+        }
     }
 }
--- CapitalizeEachLetter/Program.cs
+++ CapitalizeEachLetter/Program.cs
@@ -10,6 +10,21 @@ namespace CapitalizeEachLetter
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter some text: ");
+            string text = Console.ReadLine();
+            try
+            {
+                string newText = text.CapitalizeLetters();
+                Console.WriteLine(newText);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
